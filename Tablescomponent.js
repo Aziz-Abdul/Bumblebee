e5df634c327ab9fdb3eb987d3a3885154d7e78ae
Abdul-Aziz <54721014+Aziz-Abdul@users.noreply.github.com>
2020-10-06 17:50:46 +0000

    second commit

---
+++ Tablescomponent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function FirstComponent() {
+    return(
+  <table>
+    <tr>
+       <th>Name</th>
+       <th>Gender</th>
+       <th> Country</th>
+    </tr>
+    <tr>
+       <td>J.k Duku</td>
+       <td>Male</td>
+       <td>West Indies</td>
+    </tr>
+    <tr>
+       <td>Aryitey Canada</td>
+       <td> male</td>
+       <td> Canana</td>
+    </tr>
+    <tr>
+        <td>Barbara Manly-Spain</td>
+        <td> Female</td>
+        <td>Seville</td>
+    </tr>
+
+  </table>
+      )
+}
+
+export default FirstComponent;
