ae27441a34d50b5c36c688467c02e13b93bd58f0
Abdul-Aziz <54721014+Aziz-Abdul@users.noreply.github.com>
2020-10-07 00:59:48 +0000

    third commit

--- src/Tablescomponent.js
+++ src/Tablescomponent.js
@@ -9,25 +9,11 @@ function Tablescomponent(){
         <th>Gender</th>
         <th> Country</th>
      </tr>
-
      <tr>
        <td>J.k Duku</td>
        <td>Male</td>
        <td>West Indies</td>
-      </tr>
-
-      <tr>
-       <td>Aryeetey Canada</td>
-       <td>Male</td>
-       <td>Canada</td>
      </tr>
-
-     <tr>
-       <td>Ransford France</td>
-       <td>Male</td>
-       <td>Monaco</td>
-     </tr>
-
      </table>
       
      
